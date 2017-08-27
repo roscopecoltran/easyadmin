a412b62611eef7fad7caaf73919f9a356540caf0
tutu <gongxinong@gmail.com>
2017-08-27 22:42:04 +0800

    package

--- src/main/java/com/easyadmin/CorsConfig.java
+++ src/main/java/com/easyadmin/CorsConfig.java
@@ -3,17 +3,28 @@ package com.easyadmin;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api,/schemas")
                 .allowedOrigins("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT","OPTIONS");
+    }
+
+    @Override
+    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/data-hive/**").
+                addResourceLocations("classpath:/static/data-hive");
+        registry.addResourceHandler("/settings/**").
+                addResourceLocations("classpath:/static/settings");
     }
 
 }
